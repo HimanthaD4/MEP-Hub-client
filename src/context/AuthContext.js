d206deae4e753e3e384b524dbc1651746a93731e
Himantha Hirushan <himanthad4@gmail.com>
2025-04-09 08:12:30 +0530

    update auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,10 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 const AuthContext = createContext();
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
