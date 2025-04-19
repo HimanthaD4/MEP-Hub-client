3e24e9d944e57966ba2eaa0008603aa25183553d
Himantha Hirushan <himanthad4@gmail.com>
2025-04-19 16:06:08 +0530

    update categories Section frontend

--- src/components/home/CategoriesSection.js
+++ src/components/home/CategoriesSection.js
@@ -156,7 +156,7 @@ const CategoriesSection = ({
             <svg style={styles.categoryIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#1a56db">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
             </svg>
-            <h3 style={styles.categoryTitle}>Consultanting Firms</h3>
+            <h3 style={styles.categoryTitle}>Consulting Firms</h3>
             <div style={styles.categoryCount}>{consultantFirms.length}+ Verified Firms</div>
             <p style={styles.categoryDescription}>
               Find the perfect MEP consulting partner for your project from our network of top-rated professionals.
